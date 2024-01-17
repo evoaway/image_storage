16189bf3e91554aa4515a0c6c9f616e81b89ba4d
evoaway <anatolii.mandzynets@gmail.com>
2024-01-17 21:19:23 +0200

    Added README.md and changed controllers.py

--- controllers.py
+++ controllers.py
@@ -1,17 +1,1 @@
-# from app import mysql
-#
-#
-# def get_user(name, password):
-#     cur = mysql.connection.cursor()
-#     cur.execute('''SELECT * FROM users WHERE name = %s and password = %s''', (name, password))
-#     data = cur.fetchall()
-#     cur.close()
-#     return data
-#
-#
-# def add_user(name, password):
-#     cur = mysql.connection.cursor()
-#     cur.execute('''INSERT INTO table_name (name, age) VALUES (%s, %s)''', (name, password))
-#     mysql.connection.commit()
-#     cur.close()
-#     return 'Data added successfully'
+
